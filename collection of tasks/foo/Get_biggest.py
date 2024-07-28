22f642b867c859ef127012347d3aff98c1ebc64d
ohiimark <mainmail.00@ya.ru>
2024-07-28 20:02:22 +0300

    first commit

--- collection of tasks/foo/Get_biggest.py
+++ collection of tasks/foo/Get_biggest.py
@@ -11,31 +11,5 @@ def get_biggest(numbers):
 
         return int(''.join(loc_numbers_sorted))
 
-def get_biggest_1(numbers):
-    if not numbers:
-        return -1
-
-    else:
-        loc_numbers_str = list(map(str, numbers))
-        max_len_el = max(map(lambda x: len(x), loc_numbers_str))
-
-        loc_numbers_sorted = sorted(loc_numbers_str, key=lambda x: str(
-            x)*max_len_el if len(str(x)) <= max_len_el else str(x), reverse=True)
-
-        return int(''.join(loc_numbers_sorted))
-
-def get_biggest_2(numbers):
-    if not numbers:
-        return -1
-
-    else:
-        loc_numbers_str = list(map(str, numbers))
-        max_len_el = max(map(lambda x: len(x), loc_numbers_str))
-
-        loc_numbers_sorted = sorted(loc_numbers_str, key=lambda x: str(
-            x)*max_len_el if len(str(x)) <= max_len_el else str(x), reverse=True)
-
-        return int(''.join(loc_numbers_sorted))
 
 print(get_biggest([123, 33, 4, 43]))
-
